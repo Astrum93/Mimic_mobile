bcd4eb7e74da00cc6bd4e4548b5a8849b74d38ef
Astrum93 <jungmin2670@gmail.com>
2024-01-27 18:41:08 +0900

    240127 화면이 가로모드일 때와 세로모드일 때 다른 화면으로 빌드

--- lib/main/home/home_screen.dart
+++ lib/main/home/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mimic_mobile/common/constant/app_colors.dart';
+import 'package:mimic_mobile/main/home/vertical_home.dart';
 
-import '../../common/widget/grey_button.dart';
+import 'horizontal_home.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -74,131 +75,12 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 8, right: 8),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    width: 350,
-                    decoration: BoxDecoration(
-                      color: Colors.black26,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      padding: const EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const GreyButton(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Colors.black,
-                              size: 40,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          const GreyButton(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Colors.black,
-                              size: 40,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          const GreyButton(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Colors.black,
-                              size: 40,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          GreyButton(
-                            child: Image.asset(
-                              'assets/image/m.png',
-                              scale: 4,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          const GreyButton(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Colors.black,
-                              size: 40,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          GreyButton(
-                            child: Image.asset(
-                              'assets/image/m.png',
-                              scale: 4,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          const GreyButton(
-                            child: Icon(
-                              Icons.play_arrow,
-                              color: Colors.black,
-                              size: 40,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                          GreyButton(
-                            child: Image.asset(
-                              'assets/image/m.png',
-                              scale: 4,
-                            ),
-                          ),
-                          const SizedBox(width: 5),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    width: 400,
-                    height: 250,
-                    decoration: const BoxDecoration(
-                      color: Colors.transparent,
-                      image: DecorationImage(
-                        image: AssetImage('assets/image/sing2.png'),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: -30,
-                  top: 200,
-                  child: Image.asset(
-                    'assets/image/mimic_beam.png',
-                    scale: 5,
-                  ),
-                ),
-                Positioned(
-                  right: -30,
-                  top: 200,
-                  child: Image.asset(
-                    'assets/image/mimic_beam2.png',
-                    scale: 5,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return orientation == Orientation.portrait
+              ? VerticalHome()
+              : HorizontalHome();
+        },
       ),
     );
   }
---
+++ lib/main/home/vertical_home.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+
+import '../../common/widget/grey_button.dart';
+
+class VerticalHome extends StatefulWidget {
+  const VerticalHome({super.key});
+
+  @override
+  State<VerticalHome> createState() => _VerticalHomeState();
+}
+
+class _VerticalHomeState extends State<VerticalHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.black26,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const GreyButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        const GreyButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        const GreyButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        GreyButton(
+                          child: Image.asset(
+                            'assets/image/m.png',
+                            scale: 4,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        const GreyButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        GreyButton(
+                          child: Image.asset(
+                            'assets/image/m.png',
+                            scale: 4,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        const GreyButton(
+                          child: Icon(
+                            Icons.play_arrow,
+                            color: Colors.black,
+                            size: 40,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                        GreyButton(
+                          child: Image.asset(
+                            'assets/image/m.png',
+                            scale: 4,
+                          ),
+                        ),
+                        const SizedBox(width: 5),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 10,
+                left: 0,
+                right: 0,
+                child: Container(
+                  width: 400,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      image: AssetImage('assets/image/sing2.png'),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: -30,
+                top: 200,
+                child: Image.asset(
+                  'assets/image/mimic_beam.png',
+                  scale: 5,
+                ),
+              ),
+              Positioned(
+                right: -30,
+                top: 200,
+                child: Image.asset(
+                  'assets/image/mimic_beam2.png',
+                  scale: 5,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
