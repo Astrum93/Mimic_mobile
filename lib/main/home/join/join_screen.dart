c5f8dcc7111ebcef7e870001498fbaf5f6465907
Astrum93 <jungmin2670@gmail.com>
2024-01-29 23:04:27 +0900

    240129 회원가입 화면 구성

--- lib/main/home/join/join_screen.dart
+++ lib/main/home/join/join_screen.dart
@@ -15,14 +15,17 @@ class _JoinScreenState extends State<JoinScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            GestureDetector(
-              onTap: () {},
-              child: Image.asset(
-                'assets/image/mimic.png',
-                scale: 4,
+            Align(
+              alignment: Alignment.topLeft,
+              child: GestureDetector(
+                onTap: () {},
+                child: Image.asset(
+                  'assets/image/mimic.png',
+                  scale: 4,
+                ),
               ),
             ),
-            const SizedBox(height: 80),
+            const SizedBox(height: 10),
             const Text(
               '이메일 인증으로 안전하게 시작하기',
               style: TextStyle(
@@ -33,30 +36,6 @@ class _JoinScreenState extends State<JoinScreen> {
             const SizedBox(height: 20),
             Row(
               children: [
-                TextFormField(
-                  keyboardType: TextInputType.text,
-                  obscureText: false,
-                  cursorColor: Colors.black,
-                  style: const TextStyle(color: Colors.black),
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    hintText: '이메일 주소 입력',
-                    hintStyle: TextStyle(color: Colors.black),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
-                      ),
-                    ),
-                  ),
-                ),
                 Container(
                   color: AppColors.mainColor,
                   width: 50,
