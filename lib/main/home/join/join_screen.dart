bf0682a74969ff9564610fd6c53f88e232618cda
Astrum93 <jungmin2670@gmail.com>
2024-01-29 23:08:08 +0900

    240129 회원가입 화면 구성

--- lib/main/home/join/join_screen.dart
+++ lib/main/home/join/join_screen.dart
@@ -12,38 +12,59 @@ class _JoinScreenState extends State<JoinScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.topLeft,
-              child: GestureDetector(
-                onTap: () {},
-                child: Image.asset(
-                  'assets/image/mimic.png',
-                  scale: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: GestureDetector(
+                  onTap: () {},
+                  child: Image.asset(
+                    'assets/image/mimic.png',
+                    scale: 4,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              '이메일 인증으로 안전하게 시작하기',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 35,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Container(
-                  color: AppColors.mainColor,
-                  width: 50,
-                  height: 50,
+              const SizedBox(height: 10),
+              const Text(
+                '이메일 인증으로 안전하게 시작하기',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: AppColors.mainColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '전송',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
