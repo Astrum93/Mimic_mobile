c469b18125069d8d1dc432cb94acd864f63c9183
Astrum93 <jungmin2670@gmail.com>
2024-01-30 22:08:27 +0900

    240130 화면 방향에 따라 다르게 빌드

--- lib/main/home/join/join_screen.dart
+++ lib/main/home/join/join_screen.dart
@@ -14,98 +14,101 @@ class _JoinScreenState extends State<JoinScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topLeft,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => HomeScreen()));
-                  },
-                  child: Image.asset(
-                    'assets/image/mimic.png',
-                    scale: 4,
+      body: OrientationBuilder(builder: (context, orientation) {
+        return SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => HomeScreen()));
+                    },
+                    child: Image.asset(
+                      'assets/image/mimic.png',
+                      scale: 4,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                '이메일 인증으로 안전하게 시작하기',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
+                SizedBox(
+                    height: orientation == Orientation.portrait ? 200 : 10),
+                Text(
+                  '이메일 인증으로 안전하게 시작하기',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: orientation == Orientation.portrait ? 20 : 35,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: MediaQuery.of(context).size.width - 100,
-                child: Row(
-                  children: [
-                    Flexible(
-                      child: TextFormField(
-                        keyboardType: TextInputType.text,
-                        obscureText: false,
-                        cursorColor: Colors.black,
-                        style: const TextStyle(color: Colors.black),
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          hintText: '이메일 주소 입력',
-                          hintStyle: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 5),
-                    GestureDetector(
-                      onTap: () {
-                        debugPrint('전송');
-                      },
-                      child: Container(
-                        width: 70,
-                        height: 70,
-                        decoration: BoxDecoration(
-                          color: AppColors.mainColor,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: const Center(
-                          child: Text(
-                            '전송',
-                            style: TextStyle(
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 100,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          obscureText: false,
+                          cursorColor: Colors.black,
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            hintText: '이메일 주소 입력',
+                            hintStyle: TextStyle(
                               color: Colors.black,
-                              fontSize: 22,
                               fontWeight: FontWeight.bold,
                             ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          debugPrint('전송');
+                        },
+                        child: Container(
+                          width: 70,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: AppColors.mainColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              '전송',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
